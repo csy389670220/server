6dc2109b23f4fb39472bc3502ea8f4f3f9a64bb3
chengsiyu_fb <389670220@qq.com>
2019-07-26 09:19:07 +0800

    更新sql文件

--- cms/sql/cms.sql
+++ cms/sql/cms.sql
@@ -10,10 +10,109 @@ Target Server Type    : MYSQL
 Target Server Version : 80012
 File Encoding         : 65001
 
-Date: 2019-07-10 17:24:51
+Date: 2019-07-25 16:07:00
 */
 
 SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for t_item
+-- ----------------------------
+DROP TABLE IF EXISTS `t_item`;
+CREATE TABLE `t_item` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `name` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '' COMMENT '商品名称',
+  `price` decimal(12,2) NOT NULL DEFAULT '0.00' COMMENT '价格',
+  `img_url` varchar(120) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '' COMMENT '商品图片链接',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `update_time` datetime DEFAULT NULL COMMENT '更新时间',
+  `create_user` int(11) DEFAULT NULL COMMENT '创建人',
+  `update_user` int(11) DEFAULT NULL COMMENT '更新人',
+  `item_status` int(2) DEFAULT NULL COMMENT '状态：上架，下架，无货。。。',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Table structure for t_item_stock
+-- ----------------------------
+DROP TABLE IF EXISTS `t_item_stock`;
+CREATE TABLE `t_item_stock` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `item_id` int(11) NOT NULL COMMENT '商品ID',
+  `stock` int(21) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `1` (`item_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Table structure for t_permission
+-- ----------------------------
+DROP TABLE IF EXISTS `t_permission`;
+CREATE TABLE `t_permission` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `PARENT_ID` int(11) DEFAULT NULL COMMENT '父类ID',
+  `PER_CODE` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '权限code值',
+  `PER_DESC` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '描述',
+  `CREATE_TIME` datetime DEFAULT NULL COMMENT '创建时间',
+  `PER_LEV` int(2) NOT NULL COMMENT '资源等级',
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for t_role
+-- ----------------------------
+DROP TABLE IF EXISTS `t_role`;
+CREATE TABLE `t_role` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `ROLE_NAME` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '角色名',
+  `ROLE_DESC` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '描述',
+  `ROLE_STATUS` varchar(2) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用户状态：1-有效，0-失效，2-其他',
+  `CREATE_TIME` datetime DEFAULT NULL COMMENT '创建时间',
+  `CREATE_USER` int(11) DEFAULT NULL COMMENT '创建人',
+  `UPDATE_TIME` datetime DEFAULT NULL COMMENT '更新时间',
+  `UPDATE_USER` int(11) DEFAULT NULL COMMENT '更新人',
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=44 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for t_role_per
+-- ----------------------------
+DROP TABLE IF EXISTS `t_role_per`;
+CREATE TABLE `t_role_per` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `ROLE_ID` int(11) NOT NULL COMMENT '角色ID',
+  `PER_ID` int(11) NOT NULL COMMENT '权限ID',
+  `CREATE_TIME` datetime DEFAULT NULL COMMENT '创建时间',
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=135 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for t_seckill
+-- ----------------------------
+DROP TABLE IF EXISTS `t_seckill`;
+CREATE TABLE `t_seckill` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `item_id` int(11) NOT NULL COMMENT '商品ID',
+  `create_time` datetime NOT NULL COMMENT '创建时间',
+  `start_time` datetime NOT NULL COMMENT '开始时间',
+  `end_time` datetime NOT NULL COMMENT '结束时间',
+  `price` decimal(10,2) NOT NULL COMMENT '秒杀价格',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `item_id` (`item_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Table structure for t_success_skill
+-- ----------------------------
+DROP TABLE IF EXISTS `t_success_skill`;
+CREATE TABLE `t_success_skill` (
+  `skill_id` int(11) NOT NULL COMMENT '秒杀活动ID',
+  `user_id` int(11) NOT NULL COMMENT '用户ID',
+  `status` int(2) DEFAULT NULL COMMENT '状态',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  PRIMARY KEY (`skill_id`,`user_id`),
+  KEY `idx_create_time` (`create_time`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='秒杀记录表';
 
 -- ----------------------------
 -- Table structure for t_sys_user
@@ -32,45 +131,16 @@ CREATE TABLE `t_sys_user` (
   `UPDATE_TIME` datetime DEFAULT NULL COMMENT '更新时间',
   `UPDATE_USER` int(11) DEFAULT NULL COMMENT '更新人',
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8;
 
-
-CREATE TABLE `t_role` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID',
-  `ROLE_NAME` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '角色名',
-  `DESC` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '描述',
-  `ROLE_STATUS` varchar(2) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用户状态：1-有效，0-失效，2-其他',
-  `CREATE_TIME` datetime DEFAULT NULL COMMENT '创建时间',
-  `CREATE_USER` int(11) DEFAULT NULL COMMENT '创建人',
-  `UPDATE_TIME` datetime DEFAULT NULL COMMENT '更新时间',
-  `UPDATE_USER` int(11) DEFAULT NULL COMMENT '更新人',
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8;
-
-
-CREATE TABLE `t_permission` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID',
-  `PARENT_ID` int(11)  COMMENT '父类ID',
-  `PER_CODE` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '权限code值',
-  `ROLE_DESC` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '描述',
-  `CREATE_TIME` datetime DEFAULT NULL COMMENT '创建时间',
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8;
-
-
+-- ----------------------------
+-- Table structure for t_user_role
+-- ----------------------------
+DROP TABLE IF EXISTS `t_user_role`;
 CREATE TABLE `t_user_role` (
   `ID` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID',
-  `USER_ID` int(11)  COMMENT '用户ID',
-  `ROLE_ID` int(11)  COMMENT '角色ID',
+  `USER_ID` int(11) DEFAULT NULL COMMENT '用户ID',
+  `ROLE_ID` int(11) DEFAULT NULL COMMENT '角色ID',
   `CREATE_TIME` datetime DEFAULT NULL COMMENT '创建时间',
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8;
-
-
-CREATE TABLE `t_role_per` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID',
-  `ROLE_ID` int(11)  COMMENT '角色ID',
-  `PER_ID` int(11)  COMMENT '权限ID',
-  `CREATE_TIME` datetime DEFAULT NULL COMMENT '创建时间',
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=utf8;
