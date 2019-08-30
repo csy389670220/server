36996d73f402584a56cd58d0b124ca80e35ef6c3
liuteng <liuteng0524@126.com>
2019-08-30 15:13:47 +0800

    减少字段长度，解决以下问题 [HY000][1709] Index column size too large. The maximum column size is 767 bytes.

--- springboot+shiro+redis+日志设置/spring-boot-shiro-10/src/main/resources/sql/user_info.sql
+++ springboot+shiro+redis+日志设置/spring-boot-shiro-10/src/main/resources/sql/user_info.sql
@@ -24,9 +24,9 @@ DROP TABLE IF EXISTS `user_info`;
 CREATE TABLE `user_info` (
   `uid` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(50) DEFAULT '' COMMENT '用户名',
-  `password` varchar(256) DEFAULT NULL COMMENT '登录密码',
-  `name` varchar(256) DEFAULT NULL COMMENT '用户真实姓名',
-  `id_card_num` varchar(256) DEFAULT NULL COMMENT '用户身份证号',
+  `password` varchar(200) DEFAULT NULL COMMENT '登录密码',
+  `name` varchar(200) DEFAULT NULL COMMENT '用户真实姓名',
+  `id_card_num` varchar(200) DEFAULT NULL COMMENT '用户身份证号',
   `state` char(1) DEFAULT '0' COMMENT '用户状态：0:正常状态,1：用户被锁定',
   PRIMARY KEY (`uid`),
   UNIQUE KEY `username` (`username`) USING BTREE,
